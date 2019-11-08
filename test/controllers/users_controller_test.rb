80cfd36cb5ade7401b8ff6ca58e772f3b51ab661
Loemhuo <l.lay.sc.kh@gmail.com>
2019-11-08 16:02:33 +0700

    Add account activation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -39,15 +39,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
-
-
-  test "should not allow the admin attribute to be edited via the web" do
-    log_in_as(@other_user)
-    assert_not @other_user.admin?
-    patch user_path(@other_user), params: {
-                                    user: { password:              @other_user.password,
-                                            password_confirmation: @other_user.password_confirmation,
-                                            admin: @other_user.admin } }
-    assert_not @other_user.@other_user.admin?
-  end
 end
