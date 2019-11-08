e7b58ba5d6b36bccb0f3f504c176721317ecd449
Loemhuo <l.lay.sc.kh@gmail.com>
2019-11-08 16:46:17 +0700

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://loemhuoonruby.herokuapp.com/.herokuapp.com'
+  host = 'loemhuoonruby.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
